6755939a8f69ddc84a0786d6ce11f3418c66c72c
crosstyan <crosstyan@outlook.com>
2023-10-27 19:11:41 +0800

    a simple stupid pub

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
+import Codec.Binary.UTF8.String (encode)
+import Control.Monad (guard)
+import Data.ByteString qualified as BS
+import Data.ByteString.Lazy qualified as LBS
+import Data.Maybe (isJust)
+import Data.Text qualified
+import GHC.Stack (pushCallStack)
+import Network.MQTT.Client qualified as MC
+import Network.MQTT.Client qualified as Network.MQTT
+import Network.MQTT.Topic (mkTopic)
+import Network.URI (parseURI)
+import Prelude
+
+publishWithString :: Network.MQTT.MQTTClient -> String -> String -> Bool -> IO ()
+publishWithString c t p = do
+  case mkTopic $ Data.Text.pack t of
+    Nothing -> error "Invalid topic"
+    Just topic -> do
+      let payload = LBS.pack $ encode p
+      MC.publish c topic payload
+
 main :: IO ()
-main = do {
-   putStrLn "Hello, Haskell!"
-}
+main = do
+  let mUrl = parseURI "mqtt://weihua-iot.cn:1883"
+  guard $ isJust mUrl
+  let Just url = mUrl
+  mc <- MC.connectURI MC.mqttConfig url
+  publishWithString mc "test" "hello" False
