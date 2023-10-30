bf05d3816923c800edcb98f37e26ab9168d78602
crosstyan <crosstyan@outlook.com>
2023-10-30 16:46:37 +0800

    simple parser?

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiWayIf #-}
 -- https://ghc.gitlab.haskell.org/ghc/doc/users_guide/exts/overloaded_strings.html
 -- https://limperg.de/ghc-extensions/
 -- https://lexi-lambda.github.io/blog/2018/02/10/an-opinionated-guide-to-haskell-in-2018/#any-flavor-you-like
@@ -15,22 +16,20 @@ module Main where
 import Codec.Binary.UTF8.String (decode, encode)
 import Control.Exception (throw, try)
 import Control.Logger.Simple
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Except
 import Data.Binary.Get
 import Data.ByteString qualified as B
 import Data.ByteString.Lazy qualified as BL
-import Data.List (intercalate)
 import Data.Maybe (catMaybes, fromJust)
 import Data.Text qualified as T
+import Data.Word
 import Flow
 import Network.MQTT.Client (MQTTConfig (_msgCB))
 import Network.MQTT.Client qualified as MC
 import Network.MQTT.Topic (Topic (unTopic), mkFilter, mkTopic)
 import Network.URI (parseURI)
 import Prelude hiding (log)
-import Control.Monad (unless)
-import Control.Monad.Trans.Except
-import Data.Word
-import Data.Binary.Get
 
 accCoef :: Double
 accCoef = 1 / 32768 * 16
@@ -40,6 +39,21 @@ gyroCoef = 1 / 32768 * 2000
 
 angleCoef :: Double
 angleCoef = 1 / 32768 * 180
+
+witHeader :: Word8
+witHeader = 0x55
+gyroFlag :: Word8
+gyroFlag = 0x61
+regFlag :: Word8
+regFlag = 0x71
+
+temperatureReg :: Word8
+temperatureReg = 0x40
+quaternionReg :: Word8
+quaternionReg = 0x51
+magneticReg :: Word8
+magneticReg = 0x3A
+
 
 -- https://wit-motion.yuque.com/wumwnr/docs/gpare3
 data GyroData = GyroData
@@ -88,16 +102,67 @@ data WitData = Gyro GyroData | Magnetic MagneticData | Quaternion QuaternionData
 
 data ParseException = BadHeader | BadFlag | BadLength deriving (Show, Eq, Enum)
 
-witHeader :: Word8
-witHeader = 0x55
+-- https://stackoverflow.com/questions/9722689/haskell-how-to-map-a-tuple
+map4Tuple :: (a -> b) -> (a, a, a, a) -> (b, b, b, b)
+map4Tuple f (a, b, c, d) = (f a, f b, f c, f d)
+
+map3Tuple :: (a -> b) -> (a, a, a) -> (b, b, b)
+map3Tuple f (a, b, c) = (f a, f b, f c)
 
 -- https://hackage.haskell.org/package/parsec
 -- https://wiki.haskell.org/Dealing_with_binary_data
 -- https://www.haskellforall.com/2021/05/the-trick-to-avoid-deeply-nested-error.html
-decodeWitData :: Get (Either ParseException WitData)
-decodeWitData = do
-  header <- getWord8
-  flag <- getWord8
+-- https://www.reddit.com/r/haskell/comments/koyv6g/syntax_for_early_return_in_donotation_ghc_plugin/
+decodeData :: Get (Either ParseException WitData)
+decodeData = runExceptT $ do
+  header <- lift getWord8
+  if header /= witHeader
+    then except $ Left BadHeader
+    else do
+      flag <- lift getWord8
+      if flag == gyroFlag
+        then do
+          d <- lift decodeGyroData
+          lift $ pure $ Gyro d
+        else
+          if flag == regFlag
+            then do
+              reg <- lift getWord8
+              _ <- lift getWord8
+              if
+                | reg == quaternionReg -> do
+                    d <- lift decodeQuaternionData
+                    lift $ pure $ Quaternion d
+                | reg == magneticReg -> do
+                    d <- lift decodeMagneticData
+                    lift $ pure $ Magnetic d
+                | reg == temperatureReg -> do
+                    d <- lift decodeTemperatureData
+                    lift $ pure $ Temperature d
+                | otherwise -> except $ Left BadFlag
+            else except $ Left BadFlag
+
+decodeQuaternionData :: Get QuaternionData
+decodeQuaternionData = do
+  w <- getWord16le
+  x <- getWord16le
+  y <- getWord16le
+  z <- getWord16le
+  pure
+    $ QuaternionData
+      { quaternion = (w, x, y, z) |> map4Tuple ((* quaternionCoef) . fromIntegral)
+      }
+
+decodeTemperatureData :: Get TemperatureData
+decodeTemperatureData = do
+  temp <- getWord16le
+  pure
+    $ TemperatureData
+      { temperature = fromIntegral temp * tempCoef
+      }
+
+decodeGyroData :: Get GyroData
+decodeGyroData = do
   accX <- getWord16le
   accY <- getWord16le
   accZ <- getWord16le
@@ -107,12 +172,23 @@ decodeWitData = do
   roll <- getWord16le
   pitch <- getWord16le
   yaw <- getWord16le
+  pure
+    $ GyroData
+      { acc = (accX, accY, accZ) |> map3Tuple ((* accCoef) . fromIntegral)
+      , gyro = (angleX, angleY, angleZ) |> map3Tuple ((* gyroCoef) . fromIntegral)
+      , angleAcc = (roll, pitch, yaw) |> map3Tuple ((* angleCoef) . fromIntegral)
+      }
 
-  return $ Right $ Gyro GyroData
-    { acc = (fromIntegral accX * accCoef, fromIntegral accY * accCoef, fromIntegral accZ * accCoef)
-    , gyro = (fromIntegral angleX * gyroCoef, fromIntegral angleY * gyroCoef, fromIntegral angleZ * gyroCoef)
-    , angleAcc = (fromIntegral roll * angleCoef, fromIntegral pitch * angleCoef, fromIntegral yaw * angleCoef)
-    }
+decodeMagneticData :: Get MagneticData
+decodeMagneticData = do
+  _ <- getWord8
+  magX <- getWord16le
+  magY <- getWord16le
+  magZ <- getWord16le
+  pure
+    $ MagneticData
+      { magnetic = (magX, magY, magZ) |> map3Tuple ((* magneticCoef) . fromIntegral)
+      }
 
 -- https://zenn.dev/tobi462/articles/4ae7658d126054
 -- https://stackoverflow.com/questions/15441956/how-do-i-make-a-do-block-return-early
